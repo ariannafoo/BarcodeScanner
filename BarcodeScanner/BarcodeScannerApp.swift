11564a63c7ccfd97672bf5c3022dbe92688f0f64
Arianna Foo <fooar@sheridancollege.ca>
2025-04-08 13:53:24 -0400

    Initial Commit

---
+++ BarcodeScanner/BarcodeScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BarcodeScannerApp.swift
+//  BarcodeScanner
+//
+//  Created by Arianna Foo on 2025-04-08.
+//
+
+import SwiftUI
+
+@main
+struct BarcodeScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
