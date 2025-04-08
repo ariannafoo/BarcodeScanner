e906863f52f581b232df5d06f23ac6699f2368a2
Arianna Foo <fooar@sheridancollege.ca>
2025-04-08 16:51:59 -0400

    Updated BarcodeScannerView

---
+++ BarcodeScanner/ScannerView.swift
@@ -0,0 +1,18 @@
+//
+//  ScannerView.swift
+//  BarcodeScanner
+//
+//  Created by Arianna Foo on 2025-04-08.
+//
+
+import SwiftUI
+
+struct ScannerView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ScannerView()
+}
