1488a2c94cc6a6f1844546a16c45ba599cb88877
Arianna Foo <fooar@sheridancollege.ca>
2025-04-08 15:14:47 -0400

    ScannerVC implemented with errror handling

--- BarcodeScanner/BarcodeScannerView.swift
+++ BarcodeScanner/BarcodeScannerView.swift
@@ -9,13 +9,28 @@ import SwiftUI
 
 struct BarcodeScannerView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+        NavigationView {
+            VStack {
+
+                // Placeholder
+                Rectangle()
+                    .frame(maxWidth: .infinity, maxHeight: 300)
+                
+                Spacer()
+                    .frame(height: 60)
+                
+                Label("Scanned Barcode: ", systemImage: "barcode.viewfinder")
+                    .font(.title)
+                
+                Text("Not Yet Scanned")
+                    .bold()
+                    .font(.largeTitle)
+                    .foregroundStyle(.green)
+                    .padding()
+                
+            } // VStack
+            .navigationTitle("Barcode Scanner")
+        } // NavigationView
     }
 }
 
--- BarcodeScanner/ScannerVC.swift
+++ BarcodeScanner/ScannerVC.swift
@@ -5,4 +5,109 @@
 //  Created by Arianna Foo on 2025-04-08.
 //
 
-import Foundation
+import UIKit
+import AVFoundation
+
+enum CameraError: String {
+    case invalidDeviceInput = "Somethins is wrong with the camera. We are unable to capture the input."
+    case invalidScannedValue = "The value scanned is not valid. This app scans EAN-8 and EAN-13."
+}
+
+// List of commands - what functions will be a part of the delegate
+protocol ScannerVCDelegate: class {
+    func didFind(barcode: String)
+    
+    func didSurface(error: CameraError)
+}
+
+final class ScannerVC: UIViewController {
+    
+    let captureSession = AVCaptureSession() // capturing what's on the camera
+    var previewLayer: AVCaptureVideoPreviewLayer?
+    weak var scannerDelegate: ScannerVCDelegate?
+    
+    init(scannerDelegate: ScannerVCDelegate) {
+        super.init(nibName: nil, bundle: nil) // designated initializer for VC
+        self.scannerDelegate = scannerDelegate
+    }
+    
+    required init?(coder: NSCoder) { fatalError("init(coder:) has not been implemented") }
+    
+    private func setupCaptureSession() {
+        // is there a device that can capture video
+        guard let videoCaptureDevice = AVCaptureDevice.default(for: .video) else {
+            scannerDelegate?.didSurface(error: .invalidDeviceInput)
+            return
+        }
+        
+        let videoInput: AVCaptureDeviceInput
+        
+        do {
+            try videoInput = AVCaptureDeviceInput(device: videoCaptureDevice)
+        } catch {
+            scannerDelegate?.didSurface(error: .invalidDeviceInput)
+            return
+        }
+        
+        // Input
+        // make sure we can get input from the device
+        if captureSession.canAddInput(videoInput) {
+            captureSession.addInput(videoInput)
+        } else {
+            scannerDelegate?.didSurface(error: .invalidDeviceInput)
+            return
+        }
+        
+        // Output
+        // what actually gets scanned
+        let metaDataOutput = AVCaptureMetadataOutput()
+        if captureSession.canAddOutput(metaDataOutput) {
+            captureSession.addOutput(metaDataOutput)
+            metaDataOutput.setMetadataObjectsDelegate(self, queue: DispatchQueue.main)
+            // telling the camera to look for these barcodes
+            metaDataOutput.metadataObjectTypes = [.ean8, .ean13] // stardard 8 or 13 digit barcode
+        } else {
+            scannerDelegate?.didSurface(error: .invalidDeviceInput)
+            return
+        }
+        
+        // setup preview layer to show camera
+        previewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+        previewLayer!.videoGravity = .resizeAspectFill // fill view but keep aspect ratio
+        view.layer.addSublayer(previewLayer!)
+        
+        // start running capture session so you can see the preview on the camera
+        captureSession.startRunning()
+    }
+    
+}
+
+// What happens when we scan our barcode > telling coordinator what to do
+// Coordinator will take that info and then pass it to SwiftUI
+extension ScannerVC : AVCaptureMetadataOutputObjectsDelegate {
+    
+    func metadataOutput(_ output: AVCaptureMetadataOutput, didOutput metadataObjects: [AVMetadataObject], from connection: AVCaptureConnection) {
+        
+        // Do we have a  metadataobject in our array > get it
+        guard let object = metadataObjects.first else {
+            scannerDelegate?.didSurface(error: .invalidScannedValue)
+            return }
+        
+        // has string value
+        guard let machineReadableObject = object as? AVMetadataMachineReadableCodeObject else {
+            scannerDelegate?.didSurface(error: .invalidScannedValue)
+            return
+        }
+         
+        // get string value of barcode
+        guard let barcode = machineReadableObject.stringValue else {
+            scannerDelegate?.didSurface(error: .invalidScannedValue)
+            return
+        }
+        
+        scannerDelegate?.didFind(barcode: barcode)
+        
+        
+    }
+    
+}
